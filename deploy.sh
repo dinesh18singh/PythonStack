d3ac60516d68914af61702edf9bf9cf621c94ede
dinesh18singh <talkwddinesh@gmail.com>
2026-07-11 17:39:17 +0530

    Update ARN details

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ aws cloudformation deploy \
       ProjectName=my-web-app \
       GitHubFullRepositoryId=dinesh18singh/PythonStack \
       GitHubBranchName=main \
+      ExistingCodeConnectionArn=arn:aws:codeconnections:ap-south-1:781321020517:connection/2b93553e-8f1c-46e4-868a-e166eea34ae1 \
       ContainerPort=80
